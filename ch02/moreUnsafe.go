1f31ce9b18042f4b21be6fe336077f80fb96debc
chenxi1 <chenxi1@sensetime.com>
2021-05-01 15:05:06 +0800

    update moreUnsafe.go and unsafe.go

--- ch02/moreUnsafe.go
+++ ch02/moreUnsafe.go
@@ -8,12 +8,13 @@ import (
 func main() {
 	array := [...]int{0, 1, -2, 3, 4}
 	pointer := &array[0]
-	fmt.Print(*pointer, " ")
+	fmt.Print(*pointer, " ") // 0
+	fmt.Printf("%d\n", unsafe.Sizeof(array[0])) // 8
 	memoryAddress := uintptr(unsafe.Pointer(pointer)) + unsafe.Sizeof(array[0])
 
 	for i := 0; i < len(array)-1; i++ {
 		pointer = (*int)(unsafe.Pointer(memoryAddress))
-		fmt.Print(*pointer, " ")
+		fmt.Print(*pointer, " ") // 1, -2, 3, 4
 		memoryAddress = uintptr(unsafe.Pointer(pointer)) + unsafe.Sizeof(array[0])
 	}
 	fmt.Println()
@@ -21,4 +22,10 @@ func main() {
 	fmt.Print("One more: ", *pointer, " ")
 	memoryAddress = uintptr(unsafe.Pointer(pointer)) + unsafe.Sizeof(array[0])
 	fmt.Println()
+
+	//ch02 git:(master) ✗ go run moreUnsafe.go
+	//0 8
+	//1 -2 3 4
+	//One more: 824634166976
+
 }
